c697c84e101465427479d7eb9a921f1e06a8ea08
penso996 <penso.andrea@outlook.it>
2025-01-14 17:07:55 +0100

    started working on js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// PRENDO I DATI DAL DOM
+const instructions = document.getElementById('instructions');
+const numbersList = document.getElementById('numbers-list');
+const answersForm = document.getElementById('answers-form');
+const inputGroup = document.getElementById('input-group');
+const message = document.getElementById('message');
