96b637a99774004dc583daeaaa5e05b791378a30
penso996 <penso.andrea@outlook.it>
2025-01-14 17:10:07 +0100

    working on getelement in js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // PRENDO I DATI DAL DOM
+const countdownElement = document.getElementById('countdown');
 const instructions = document.getElementById('instructions');
 const numbersList = document.getElementById('numbers-list');
 const answersForm = document.getElementById('answers-form');
 const inputGroup = document.getElementById('input-group');
 const message = document.getElementById('message');
+
