037d906c7d31b346c73f4c407e891fa69028efa7
penso996 <penso.andrea@outlook.it>
2025-01-14 17:39:37 +0100

    working on generate 5 random numbers

--- js/script.js
+++ js/script.js
@@ -6,4 +6,15 @@ const answersForm = document.getElementById('answers-form');
 const inputGroup = document.getElementById('input-group');
 const message = document.getElementById('message');
 
-// INTRODUCO LE VARIABILI DI GIOCO
+// GENERO 5 NUMERI CASUALI NON UGUALI FRA DI LORO
+const min = 1;
+const max = 50;
+const totalNumbers = 5;
+
+function generateRandomUniqueNumbers() {
+    let uniqueNumbers = new Set();
+    while (uniqueNumbers.size < totalNumbers) {
+        uniqueNumbers.add(Math.floor(Math.random() * (max - min + 1)) + min);
+    }
+    return Array.from(uniqueNumbers);
+}
